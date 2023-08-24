14c353a6b24c74ead954ac1049a2b462bd76b0ef
believeohiozua <believe.ohiozua@gmail.com>
2023-08-24 01:22:09 +0100

    Data types

---
+++ data_types.py
@@ -0,0 +1,27 @@
+# This is a single line comment in python 
+
+"""
+This is a multiple line comment in python 
+
+NOTE:
+    1. The print() function is used to display the output of our codes in the console or terminal
+    2. Comments or commented codes are not printed
+    3. Every value is represented as an object in python 
+    4. The type() function is used to get the type of any object in python
+"""
+
+# Let us print some data types we know in python
+
+print("Data type of 123 is  ", type(123)) #int
+print("Data type of 1.23 is ", type(1.23)) # float
+print("Data type of 'single quoted word' is ", type('single quoted word')) # str
+print('Data type of "Double quoted word" is ', type("Double quoted word")) #str
+print("Data type of True or False is  ", type(True)) #bool
+print("Data type of [1,2,3] is ", type([1,2,3]))#list
+print("Data type of {'a': 1, 'b': 2} is ", type({'a':1, 'b':2})) #dict
+print("Data type of None is ", type(None)) #NoneType
+
+#More codes
+print("Data type of {1,2,3,4} is ", type({1,2,3,4}))
+print("Data type of (1,2,3,)} is ", type((1,2,3,)))
+print("Data type of {'a': 1, 'b': 2, 'c': 3,'d': 4} is ", type({'a': 1, 'b': 2, 'c': 3,'d': 4}))
