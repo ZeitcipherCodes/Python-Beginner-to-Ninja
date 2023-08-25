b6cce1632e3b4f7a2764665923749bc6ad1af48a
believeohiozua <believe.ohiozua@gmail.com>
2023-08-25 14:52:43 +0100

    python variables

---
+++ variables.py
@@ -0,0 +1,34 @@
+"""
+In python, variables are used to store values.They act as placeholders of values for various data types
+"""
+#Examples of variables
+name = "Tbota"
+age = 25
+height = 5.7
+hobbies = ['coding', 'singing', 'reading']
+
+print("My name is ", name, " and I am ", age, " years old ", " my hobbies are ", hobbies)
+
+#variable reassigning
+name = "John Doe"
+age = 40
+print("My name is ", name, " and I am ", age, " years old")
+
+"""
+Rules of variable assignment in python
+1. Variable names must start with a letter (a-z, A-Z), or an underscore (_).
+2. After the first character, variable names can include letters, numbers, and or an underscore
+
+Tips:
+    1. Use all-caps to declare a constant variable
+    PI    = 3.14159
+    GRAVITY    = 9.81
+   2. Avoid using python built-in keywords as variable names.
+   To get all python reserved built-in key words
+   
+   1. open the python terminal
+   2. import keyword
+   3. print(keyword.kwlist)
+   
+   see description below for more information. Cheers
+   """
