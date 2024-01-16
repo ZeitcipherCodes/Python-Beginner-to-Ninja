c6562bddcbf6f62f4f7153e1b390c4346c186125
believeohiozua <believe.ohiozua@gmail.com>
2024-01-16 17:43:31 +0000

    python operators

---
+++ operators.py
@@ -0,0 +1,36 @@
+"""
+There are 7 major groups of operators in python 
+1. Arithmetic Operators
+2. Assignment operators
+3. comparison operators
+4. Logical Operators 
+5. Identity operators
+6. Membership Operators
+7. Bitwise Operators
+"""
+
+"""
+Arithmetic Operators:
+Arithmetic operators in Python are used to perform mathematical operations on numeric values.
+"""
+
+#Addition
+print("Adds two values together 1+2 = ", 1+2)
+
+#Subtraction
+print("Subtracts the right operand from the left operand 5-3 = ", 5-3)
+
+#Multiplication
+print("Multiplies two values 2*6 = ", 2*6)
+
+#Division
+print("Divides the left operand by the right operand 10/5 = ", 10/5)
+
+#Modulus
+print("Returns the remainder of the division of the left operand by the right operand. 10 % 3 = ", 10 % 3 )
+
+#Exponentiation
+print("Raises the left operand to the power of the right operand. 2 ** 3 = ", 2 ** 3 )
+
+#Floor Division
+print("Returns the quotient of the division, discarding any remainder, and returns an integer. 15 // 2 = ", 15 // 2)
